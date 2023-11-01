8c267d8aed31d0e4de2cf20a354289898f1f3177
Shamim <149160986+arduinoshamim@users.noreply.github.com>
2023-11-01 18:29:12 +0600

    Update sketch_oct27a.ino

--- sketch_oct27a.ino
+++ sketch_oct27a.ino
@@ -1,12 +1,21 @@
-// shamim
+//a simple arduino blink code by shamim the programmer
+// subscribe
+
+int ledPin = 13; //Define the digital pin connected to the LED
+int blinkInterval = 1000; //Interval in millisecond (1 second)
 
 void setup() {
-  pinMode(13, OUTPUT);  // Set Pin 13 as an output
+  pinMode(ledPin, OUTPUT); //Set the LED pin as an output
 }
 
 void loop() {
-  digitalWrite(13, HIGH);  // Turn the LED on
-  delay(1000);           // Wait for 1 second
-  digitalWrite(13, LOW);   // Turn the LED off
-  delay(1000);           // Wait for 1 second
+  digitalWrite(ledPin, HIGH); //Turn the LED on
+  delay(blinkInterval); //Wait for the specified Interval
+  digitalWrite(ledPin, LOW); // Turn the LED off
+  delay(blinkInterval); //Wait for the specified Interval
 }
+
+
+// With this code, you should have the LED conencted to digital pin 13 on your Arduino board, and with blink on and off ar a 1-second interval.Adjust the 'blinkInterval' variable if you want the LED to blink at a different rate.
+
+// bye bye
